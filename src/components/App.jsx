31bf009bc2a0039d6c7c8a71208b78335f87b8ab
aliabdulazeez <aliabdulazeez01@gmail.com>
2020-12-18 08:31:01 +0900

    let it begin

---
+++ src/components/App.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Heading from './Heading';
+import Entry from './Entry';
+import emojipedia from '../emojipedia';
+import Footer from './Footer';
+
+function createEmojiCard(card){
+
+  return <Entry 
+             key ={card.id}
+             img = {card.emoji}
+             name = {card.name}
+             meaning = {card.meaning}
+         ></Entry>  
+}
+
+function App() {
+  return (
+    <div>
+      
+      <Heading></Heading>
+
+      <dl className="dictionary">
+
+      {emojipedia.map(createEmojiCard)}
+
+      
+
+      
+
+
+      </dl>
+
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Entry.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+
+
+function Entry(props){
+    return(
+        <div className="term">
+        
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">{props.img}</span>
+            <span>{props.name}</span>
+          </dt>
+
+          <dd>
+            {props.meaning}
+          </dd>
+
+        </div>
+    )
+}
+
+export default Entry;  
